354795411f051bc7939b883c2c647cde87271738
bruno cardoso <brunocardoso.developer@gmail.com>
2024-11-14 17:53:45 -0300

    style: adjust style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React, { Suspense } from "react";
-import "./styles/global.css";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { ScrollButton } from "./components/ScrollButton";
+import React, { Suspense } from 'react'
+import './styles/global.css'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { ScrollButton } from './components/ScrollButton'
 
-const News = React.lazy(() => import("news/News"));
+const News = React.lazy(() => import('news/News'))
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-14 justify-between">
+    <div className="min-h-screen flex flex-col justify-between w-full">
       <Header />
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <News />
-      </Suspense>
+      <div className="flex justify-center items-center">
+        <Suspense fallback={<div>Loading...</div>}>
+          <News />
+        </Suspense>
+      </div>
 
       <ScrollButton />
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App

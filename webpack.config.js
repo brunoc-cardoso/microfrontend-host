dc3c61ca2dccdc2273ca4fe25ef236d9287ae035
Bruno Cardoso <brunocardoso.developer@gmail.com>
2024-11-14 17:43:22 -0300

    feat: add eventBus package

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.tsx',
-  mode: 'development',
+  entry: "./src/index.tsx",
+  mode: "development",
   devServer: {
     port: 3000,
     historyApiFallback: true,
     static: {
-      directory: path.resolve(__dirname, 'dist')
-    }
+      directory: path.resolve(__dirname, "dist"),
+    },
   },
   module: {
     rules: [
@@ -19,56 +19,60 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
               presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-                '@babel/preset-typescript'
-              ]
-            }
-          }
-        ]
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'host',
+      name: "host",
       remotes: {
-        news: 'news@http://localhost:3001/remoteEntry.js',
-        sponsors: 'sponsors@http://localhost:3002/remoteEntry.js',
-        ads: 'ads@http://localhost:3003/remoteEntry.js',
-        weather: 'weather@http://localhost:3004/remoteEntry.js'
+        news: "news@http://localhost:3001/remoteEntry.js",
+        sponsors: "sponsors@http://localhost:3002/remoteEntry.js",
+        ads: "ads@http://localhost:3003/remoteEntry.js",
+        weather: "weather@http://localhost:3004/remoteEntry.js",
+      },
+      exposes: {
+        "./eventBus": "./src/utils/eventBus.ts",
       },
       shared: {
         react: {
           singleton: true,
           eager: true,
-          requiredVersion: require('./package.json').dependencies.react
+          requiredVersion: require("./package.json").dependencies.react,
         },
-        'react-dom': {
+        "react-dom": {
           singleton: true,
           eager: true,
-          requiredVersion: require('./package.json').dependencies['react-dom']
-        }
-      }
+          requiredVersion: require("./package.json").dependencies["react-dom"],
+        },
+      },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
+      template: "./public/index.html",
+    }),
   ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'http://localhost:3000/'
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "http://localhost:3000/",
+    clean: true,
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
   },
-  target: 'web'
-}
+  target: "web",
+};
